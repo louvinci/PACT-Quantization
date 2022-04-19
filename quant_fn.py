0df009cc55ac4e1620f7bc020ec9d108a708d2d6
Vinci <louwenqi@mail.ustc.edu.cn>
2022-04-19 10:01:10 +0800

    modify PACT

--- quant_fn.py
+++ quant_fn.py
@@ -36,11 +36,12 @@ class ActFn(Function):
             return x
         ctx.save_for_backward(x, alpha)
         # y_1 = 0.5 * ( torch.abs(x).detach() - torch.abs(x - alpha).detach() + alpha.item() )
-        y = torch.clamp(x, min = 0, max = alpha.item())
+        #y = torch.clamp(x, min =0, max = alpha.item())
+        y = torch.clamp(x, min = -alpha.abs().item(), max = alpha.abs().item())
         scale = (2**k - 1) / alpha
         y_q = torch.round( y * scale) / scale
         return y_q
-
+    '''
     @staticmethod
     def backward(ctx, dLdy_q):
         # Backward function, I borrowed code from
@@ -53,10 +54,26 @@ class ActFn(Function):
         # dL / dy_q = argument,  dy_q / dy * dy / dalpha = 0, 1 with x value range 
         lower_bound      = x < 0
         upper_bound      = x > alpha
-        # x_range       = 1.0-lower_bound-upper_bound
         x_range = ~(lower_bound|upper_bound)
-        grad_alpha = torch.sum(dLdy_q * torch.ge(x, alpha).float()).view(-1)
+        grad_alpha = torch.sum(dLdy_q * torch.ge(x, alpha).float()).view(-1) 
         return dLdy_q * x_range.float(), grad_alpha, None
+    '''
+    @staticmethod
+    def backward(ctx, dLdy_q):
+        # Backward function, I borrowed code from
+        # https://github.com/obilaniu/GradOverride/blob/master/functional.py
+        # We get dL / dy_q as a gradient
+        x, alpha, = ctx.saved_tensors
+        # Weight gradient is only valid when [0, alpha]
+        # Actual gradient for alpha,
+        # By applying Chain Rule, we get dL / dy_q * dy_q / dy * dy / dalpha
+        # dL / dy_q = argument,  dy_q / dy * dy / dalpha = 0, 1 with x value range 
+        lower_bound      = x < -alpha.abs()
+        upper_bound      = x > alpha.abs()
+        x_range = ~(lower_bound|upper_bound)
+        grad_alpha = torch.sum( dLdy_q * (torch.ge(x, alpha.abs()).float()-torch.le(x,-alpha.abs()).float()) ).view(-1)
+        return dLdy_q * x_range.float(), grad_alpha, None
+        
 
 class ActQuant(nn.Module):
     def __init__(self,a_bit,scale_coef=10.0):
@@ -69,7 +86,8 @@ class ActQuant(nn.Module):
         if self.bit == 32:
             q_x = x
         else:
-            q_x = self.quant(x,self.scale_coef,self.bit)
+            #q_x = self.quant(x,self.scale_coef,self.bit)
+            q_x = self.quant(x,self.scale_coef,self.bit-1) #-alpha, alpha
         return q_x
 
 
